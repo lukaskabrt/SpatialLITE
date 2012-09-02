03cbe01785b996b69640a7bd58b2d096c1874b1f
Lukas <Lukas@Lukas-NTB>
2012-09-02 12:56:17 +0200

    Implementd GpxDocument

---
+++ src/SpatialLite.Gps/GpxDocument.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpatialLite.Gps.Geometries;
+using SpatialLite.Gps.IO;
+
+namespace SpatialLite.Gps {
+    /// <summary>
+    /// Represents an in-memory GPX document with it's waypoints, routes and tracks 
+    /// </summary>
+    public class GpxDocument {
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the GpxDocument class that is empty
+        /// </summary>
+        public GpxDocument() {
+            this.Waypoints = new List<GpxPoint>();
+            this.Routes = new List<GpxRoute>();
+            this.Tracks = new List<GpxTrack>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the GpxDocument class with given GPX entities
+        /// </summary>
+        /// <param name="waypoints">A collection of waypoints to add to the document.</param>
+        /// <param name="routes">A collection of routes to add to the document.</param>
+        /// <param name="tracks">A collection of tracks to add to the document.</param>
+        public GpxDocument(IEnumerable<GpxPoint> waypoints, IEnumerable<GpxRoute> routes, IEnumerable<GpxTrack> tracks) {
+            this.Waypoints = new List<GpxPoint>(waypoints);
+            this.Routes = new List<GpxRoute>(routes);
+            this.Tracks = new List<GpxTrack>(tracks);
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Gets collection of waypoints from the document.
+        /// </summary>
+        public List<GpxPoint> Waypoints { get; private set; }
+
+        /// <summary>
+        /// Gets collection of routes from the document.
+        /// </summary>
+        public List<GpxRoute> Routes { get;private  set; }
+
+        /// <summary>
+        /// Gets collection of tracks from the document.
+        /// </summary>
+        public List<GpxTrack> Tracks { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Saves content of the GpxDocument to file.
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
+        public void Save(string path) {
+            using (GpxWriter writer = new GpxWriter(path, new GpxWriterSettings() {WriteMetadata = true})) {
+                foreach (var waypoint in this.Waypoints) {
+                    writer.Write(waypoint);
+                }
+
+                foreach (var route in this.Routes) {
+                    writer.Write(route);
+                }
+
+                foreach (var track in this.Tracks) {
+                    writer.Write(track);
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Loads Gpx data from a file.
+        /// </summary>
+        /// <param name="path">Path to the GPX file.</param>
+        /// <returns>GpxDocument instance with data from GPX file</returns>
+        public static GpxDocument Load(string path) {
+            GpxDocument result = new GpxDocument();
+
+            using (GpxReader reader = new GpxReader(path, new GpxReaderSettings() { ReadMetadata = true })) {
+                IGpxGeometry geometry = null;
+                while ((geometry = reader.Read()) != null) {
+                    switch (geometry.GeometryType) {
+                        case GpxGeometryType.Waypoint: result.Waypoints.Add((GpxPoint)geometry); break;
+                        case GpxGeometryType.Route: result.Routes.Add((GpxRoute)geometry); break;
+                        case GpxGeometryType.Track: result.Tracks.Add((GpxTrack)geometry); break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Tests.SpatialLite.Gps/GpxDocumentTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using SpatialLite.Gps.IO;
+using SpatialLite.Gps;
+using SpatialLite.Gps.Geometries;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Tests.SpatialLite.Gps {
+    public class GpxDocumentTests {
+        #region Constructors tests
+
+        #region Constructor() tests
+
+        [Fact]
+        public void Constructor_CreatesEmptyDocument() {
+            var target = new GpxDocument();
+
+            Assert.Empty(target.Waypoints);
+            Assert.Empty(target.Routes);
+            Assert.Empty(target.Tracks);
+        }
+
+        #endregion
+
+        #region Constructor(Waypoints, Routes, Tracks) tests
+
+        [Fact]
+        public void Constructor_WaypointsRoutesTracks_CreatesDocumentWithGpxEntities() {
+            IEnumerable<GpxPoint> waypoints = new[] { new GpxPoint() };
+            IEnumerable<GpxRoute> routes = new[] { new GpxRoute() };
+            IEnumerable<GpxTrack> tracks = new[] { new GpxTrack() };
+
+            var target = new GpxDocument(waypoints, routes, tracks);
+
+            Assert.Equal(waypoints, target.Waypoints);
+            Assert.Equal(routes, target.Routes);
+            Assert.Equal(tracks, target.Tracks);
+        }
+
+        [Fact]
+        public void Constructor_WaypointsRoutesTracks_ThrowsArgumentNullExceptionIfWaypointsIsNull() {
+            IEnumerable<GpxRoute> routes = new[] { new GpxRoute() };
+            IEnumerable<GpxTrack> tracks = new[] { new GpxTrack() };
+
+            Assert.Throws<ArgumentNullException>(() => new GpxDocument(null, routes, tracks));
+        }
+
+        [Fact]
+        public void Constructor_WaypointsRoutesTracks_ThrowsArgumentNullExceptionIfRoutesIsNull() {
+            IEnumerable<GpxPoint> waypoints = new[] { new GpxPoint() };
+            IEnumerable<GpxTrack> tracks = new[] { new GpxTrack() };
+
+            Assert.Throws<ArgumentNullException>(() => new GpxDocument(waypoints, null, tracks));
+        }
+
+        [Fact]
+        public void Constructor_WaypointsRoutesTracks_ThrowsArgumentNullExceptionIfTracksIsNull() {
+            IEnumerable<GpxPoint> waypoints = new[] { new GpxPoint() };
+            IEnumerable<GpxRoute> routes = new[] { new GpxRoute() };
+
+            Assert.Throws<ArgumentNullException>(() => new GpxDocument(waypoints, routes, null));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region static Load(string) tests
+
+        [Fact]
+        public void Load_string_ThrowsExceptionIfPathIsNull() {
+            string path = null;
+
+            Assert.Throws<ArgumentNullException>(() => GpxDocument.Load(path));
+        }
+
+        [Fact]
+        public void Load_string_ThrowsExceptionIfFileDoesntExists() {
+            string path = "non-existing-file.gpx";
+
+            Assert.Throws<FileNotFoundException>(() => GpxDocument.Load(path));
+        }
+
+        [Fact]
+        public void Load_LoadsGpxEntitiesFromFile() {
+            string path = "../../src/Tests.SpatialLite.Gps/Data/Gpx/gpx-real-file.gpx";
+
+            var target = GpxDocument.Load(path);
+
+
+            Assert.Equal(3, target.Waypoints.Count);
+            Assert.Equal(2, target.Routes.Count);
+            Assert.Equal(1, target.Tracks.Count);
+        }
+
+        #endregion
+
+        #region Save(string) tests
+
+        [Fact]
+        public void Save_ThrowsExceptionIfPathIsNull() {
+            string path = null;
+            var target = new GpxDocument();
+
+            Assert.Throws<ArgumentNullException>(() => target.Save(path));
+        }
+
+        [Fact]
+        public void Save_SavesDataToFile() {
+            string path = "TestFiles\\gpxdocument-save-test.gpx";
+            File.Delete(path);
+
+            var target = GpxDocument.Load("../../src/Tests.SpatialLite.Gps/Data/Gpx/gpx-real-file.gpx");
+            target.Save(path);
+
+            var original = XDocument.Load("../../src/Tests.SpatialLite.Gps/Data/Gpx/gpx-real-file.gpx");
+            var saved = XDocument.Load(path);
+
+            Assert.True(XDocumentExtensions.DeepEqualsWithNormalization(original, saved, null));
+        }
+
+        #endregion
+    }
+}
