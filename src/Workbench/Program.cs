e4b93e4ff64bc0c31883479fbc5ff312f19f7040
Lukas Kabrt <lukas@kabrt.cz>
2019-03-18 23:14:42 +0100

    Benchmarks

---
+++ src/Workbench/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SpatialLite.Core.IO;
+using SpatialLite.Osm;
+using SpatialLite.Osm.Geometries;
+using SpatialLite.Osm.IO;
+
+namespace Workbench {
+    class Program {
+        static void Main(string[] args) {
+            PrepareData();
+        }
+
+        private static void PrepareData() {
+            using (var reader = new PbfReader("c:\\Temp\\azores-latest.osm.pbf", new OsmReaderSettings() { ReadMetadata = false })) {
+                var db = OsmGeometryDatabase.Load(reader, true);
+
+                using (var writer = new WktWriter("c:\\Temp\\azores.wkt", new WktWriterSettings())) {
+                    foreach (var entity in db.Nodes) {
+                        writer.Write(entity);
+                    }
+
+                    foreach (var entity in db.Ways) {
+                        writer.Write(entity);
+                    }
+                }
+            }
+        }
+    }
+}
